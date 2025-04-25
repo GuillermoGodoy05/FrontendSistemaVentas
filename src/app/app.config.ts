cb40956a2ccf788af04920d411abcddb20b79e22
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-04-25 14:30:05 -0300

    Proyecto de Angular Frontend Sistema ventas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { environment } from '../environments/environment'; // Importa el objeto environment
+
+import { provideHttpClient } from '@angular/common/http'; // Importa provideHttpClient
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    { provide: 'env', useValue: environment }, // Proporciona la configuración
+    provideHttpClient(),
+  ]
 };
+
