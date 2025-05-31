28d28c6b6f4b0423873c763dfab7f4d1e3b03d62
guillermoGodoyPro <guillermo.rgo92@gmail.com>
2025-05-30 23:20:28 -0300

    config para netlify4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    endpoint: 'http://www.sistemaventasportfolio.somee.com/api/'     
+    endpoint: 'https://www.sistemaventasportfolio.somee.com/api/'     
   };
 
   
